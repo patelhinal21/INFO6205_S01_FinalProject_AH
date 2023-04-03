ef3d40bee1aa0c7a9e4449ab88ec86969b90d2e8
aishwaryavenkatesan <venkatesan.ai@northeastern.edu>
2023-04-03 16:38:33 -0400

    adding input for complete graph

--- src/main/java/TSP/Prim/Prims.java
+++ src/main/java/TSP/Prim/Prims.java
@@ -37,20 +37,20 @@ public class Prims {
 
     public static void main(String[] args) {
         EdgeWeightedGraph edgeWeightedGraph = new EdgeWeightedGraph(9);
-        Edge e1 = new Edge(0,1,4);
-        Edge e2 = new Edge(0,7,8);
-        Edge e3 = new Edge(1,2,8);
-        Edge e4 = new Edge(1,7,11);
-        Edge e5 = new Edge(7,8,7);
-        Edge e6 = new Edge(7,6,1);
-        Edge e7 = new Edge(2,8,2);
-        Edge e8 = new Edge(2,3,7);
-        Edge e9 = new Edge(8,6,6);
-        Edge e10 = new Edge(2,5,4);
-        Edge e11 = new Edge(5,3,14);
-        Edge e12 = new Edge(5,4,10);
-        Edge e13 = new Edge(3,4,9);
-        Edge e14 = new Edge(6,5,2);
+        Edge e1 = new Edge(0,1,12);
+        Edge e2 = new Edge(0,2,10);
+        Edge e3 = new Edge(0,3,19);
+        Edge e4 = new Edge(0,4,8);
+        Edge e5 = new Edge(1,2,3);
+        Edge e6 = new Edge(1,3,7);
+        Edge e7 = new Edge(1,4,2);
+        Edge e8 = new Edge(2,3,6);
+        Edge e9 = new Edge(2,4,4);
+        Edge e10 = new Edge(3,4,4);
+//        Edge e11 = new Edge(5,3,14);
+//        Edge e12 = new Edge(5,4,10);
+//        Edge e13 = new Edge(3,4,9);
+//        Edge e14 = new Edge(6,5,2);
 
         edgeWeightedGraph.addEdge(e1);
         edgeWeightedGraph.addEdge(e2);
@@ -62,10 +62,10 @@ public class Prims {
         edgeWeightedGraph.addEdge(e8);
         edgeWeightedGraph.addEdge(e9);
         edgeWeightedGraph.addEdge(e10);
-        edgeWeightedGraph.addEdge(e11);
-        edgeWeightedGraph.addEdge(e12);
-        edgeWeightedGraph.addEdge(e13);
-        edgeWeightedGraph.addEdge(e14);
+//        edgeWeightedGraph.addEdge(e11);
+//        edgeWeightedGraph.addEdge(e12);
+//        edgeWeightedGraph.addEdge(e13);
+//        edgeWeightedGraph.addEdge(e14);
 
         Prims prims = new Prims(edgeWeightedGraph);
 
